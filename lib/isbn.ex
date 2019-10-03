388970a75fd46cdf3e55d36735d53f8d6d23655b
SimonLab <simon.labondance@gmail.com>
2019-10-03 14:28:21 +0100

    initialise elixir application

---
+++ lib/isbn.ex
@@ -0,0 +1,18 @@
+defmodule Isbn do
+  @moduledoc """
+  Documentation for Isbn.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Isbn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/isbn_test.exs
@@ -0,0 +1,8 @@
+defmodule IsbnTest do
+  use ExUnit.Case
+  doctest Isbn
+
+  test "greets the world" do
+    assert Isbn.hello() == :world
+  end
+end
